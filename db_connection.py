42f8217a94f3b924827d071e54c8641fa2fbaa56
SohaibAnwaar <sohaib.anwar@invozone.com>
2022-01-15 22:10:00 +0500

    db connections

---
+++ db_connection.py
@@ -0,0 +1,17 @@
+import psycopg2
+from psycopg2 import Error
+
+connection = psycopg2.connect(user="postgres",
+                                  password="postgres",
+                                  host="159.223.129.8",
+                                  port="5432",
+                                  database="postgres")
+
+def get_connection():
+    cursor = connection.cursor()
+    cursor.execute("select * from users")
+    record = cursor.fetchone()
+    print("User db ", record)
+    return cursor 
+
+
